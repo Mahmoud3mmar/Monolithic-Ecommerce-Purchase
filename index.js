e87ebba08187e11ca96da2ae3caa3f6886b3d754
Mahmoud3mmar <55957447+Mahmoud3mmar@users.noreply.github.com>
2023-12-13 14:23:26 +0200

    feat : added middlewares for all routes

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import express from 'express'
-import Productrouter from './Modules/Product-Catalog/routers/Product-Catalog.routes.js'
+import productRouter from './Modules/Product-Catalog/routers/Product-Catalog.routes.js'
+import orderRouter from './Modules/Order-Management/routers/Order-Management.routes.js'
+import customerRouter from './Modules/Customer/routers/Customer.routes.js'
+import cartRouter from './Modules/Shopping-Cart/routers/Shopping-Cart.routes.js'
+import authRouter from './Modules/Auth/routers/Auth.routes.js'
+
 import ConnectToDB from './DataBase/db.js'
 const app = express()
 const port = 3000
@@ -7,8 +12,12 @@ const port = 3000
 app.use(express.json())
 
 
-app.use('/products',Productrouter)
+app.use('/products',productRouter)
+app.use('/customer',customerRouter)
+app.use('/order',orderRouter)
+app.use('/cart',cartRouter)
 
+app.use('/auth',authRouter)
 
 
 app.use((error,req,res,next)=>{
